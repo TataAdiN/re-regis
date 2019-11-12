fc7a98f32f45db3082d9ad603c6420d56d4e2a63
Tata Adi Nugroho <tataa2237@gmail.com>
2019-11-12 10:04:34 +0700

    changelog -> kasih komentar

--- application/modules/student/controllers/Unduh.php
+++ application/modules/student/controllers/Unduh.php
@@ -14,7 +14,9 @@ class Unduh extends MY_Controller {
         $data['num'] = 4;
 		$data['type'] = "student";
 		$data['img'] = base_url().'img/gambar.jpg';
-		$this->ftp->download('/image/pc-android-1jutaan.jpg',getcwd().'/img/gambar.jpg','auto');
+		// ganti '/image/pc-android-1jutaan.jpg' dibawah
+		// sesuai yang di storage FreeNAS
+		$this->ftp->download('/image/pc-android-1jutaan.jpg' ,getcwd().'/img/gambar.jpg','auto');
 		$this->blade->render('unduh/student', $data);
 		$this->ftp->close();
 		//unlink(getcwd().'/img/gambar.jpg');
@@ -23,9 +25,9 @@ class Unduh extends MY_Controller {
 	public function initFtp()
     {
         $this->load->library('ftp');
-        $config['hostname'] = 'files.000webhost.com';
-        $config['username'] = 'cobaftp';
-        $config['password'] = '123123123';
+        $config['hostname'] = 'files.000webhost.com'; //ganti ini
+        $config['username'] = 'cobaftp'; //ganti ini
+        $config['password'] = '123123123'; //ganti ini
         $config['debug']    = TRUE;
         $this->ftp->connect($config);
     }
